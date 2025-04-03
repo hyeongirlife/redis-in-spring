e525965b976688d03c8a7e0a234771518799f44d
HyeonGeol <boo1996@naver.com>
2025-04-03 09:26:40 +0900

    feat: 레디스 적용된 게시글 조회 API 작성

---
+++ src/test/java/org/hyeongeol/banking/redisinspring/RedisInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeongeol.banking.redisinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisInSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
